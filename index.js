81db8a335a00841879ad2a81d58db3018cdaa5cd
MrAMB312 <a.blomarz@gmail.com>
2024-10-21 21:17:54 -0500

    complete phase 1 array filter method lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(function (driver) {
+        return driver.toLowerCase() === name.toLowerCase();
+    })
+}
+
+function fuzzyMatch(drivers, firstLetters) {
+    return drivers.filter(function (driver) {
+        return driver.toLowerCase().startsWith(firstLetters.toLowerCase());
+    })
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(function (driver) {
+        return driver.name.toLowerCase() === name.toLowerCase();
+    })
+}
